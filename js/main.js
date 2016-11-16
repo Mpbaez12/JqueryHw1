9d718d8868c088b02a72f6ec1e4039247c62d998
bye.ez <michaelbaez12@utexas.edu>
2016-11-15 19:36:03 -0600

    Added Mouse Enter and exit events
    Events control my highlight/unhighlight function

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ function highlight(){
   $('.jumbotron').addClass('highlighted');
 
 }
-
 function unhighlight(){
   $('h1').text('Hello,World!');
   $('.jumbotron').removeClass('highlighted');
 }
-
 function hide(){
   $('.jumbotron').fadeOut();
 }
 function show(){
   $('.jumbotron').slideDown();
 }
+
+$(document).ready(function() {
+  $('.jumbotron').on('mouseenter', highlight);
+});
+
+$(document).ready(function() {
+  $('.jumbotron').on('mouseleave', unhighlight);
+});
